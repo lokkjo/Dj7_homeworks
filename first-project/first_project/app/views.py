fdbdb10feec009e98688941a9c927dffd2e79634
Alexander Vinogradov <dra-katt@yandex.ru>
2019-12-08 14:42:52 +0300

    Задача 1: Создание проекта. Решение.

--- first-project/first_project/app/views.py
+++ first-project/first_project/app/views.py
@@ -1,6 +1,9 @@
 from django.http import HttpResponse
 from django.shortcuts import render, reverse
 
+from datetime import datetime
+from os import getcwd, listdir
+
 
 def home_view(request):
     template_name = 'app/home.html'
@@ -8,8 +11,8 @@ def home_view(request):
     # функцию `reverse`
     pages = {
         'Главная страница': reverse('home'),
-        'Показать текущее время': '',
-        'Показать содержимое рабочей директории': ''
+        'Показать текущее время': reverse('time'),
+        'Показать содержимое рабочей директории': reverse('workdir'),
     }
     
     # context и параметры render менять не нужно
@@ -23,13 +26,25 @@ def home_view(request):
 def time_view(request):
     # обратите внимание – здесь HTML шаблона нет, 
     # возвращается просто текст
-    current_time = None
-    msg = f'Текущее время: {current_time}'
+    date = datetime.now()
+    current_date = date.strftime('%Y-%m-%d')
+    current_time = date.strftime('%H:%M:%S')
+    msg = f'Сегодня {current_date}. ' \
+          f'<br>Текущее время: {current_time}'
     return HttpResponse(msg)
 
 
 def workdir_view(request):
+    work_dir_path = getcwd()
+    msg = f'Рабочая директория: {work_dir_path}<br><br>' \
+          f'<i>Содержимое директории:</i><br>'
+    work_dir_list = listdir(work_dir_path)
+    for item in work_dir_list:
+        file_string = f'– {item}<br>'
+        msg += file_string
     # по аналогии с `time_view`, напишите код,
     # который возвращает список файлов в рабочей 
     # директории
-    raise NotImplemented
+    if not msg:
+        raise NotImplemented
+    return HttpResponse(msg)
